763c503e2cdb5eb542b14c3c28cb0b0f09bec735
cibi <cibi_16@live.com>
2024-09-02 18:44:37 -0700

    gpu + fineweb + lr

--- fineweb_downloader.py
+++ fineweb_downloader.py
@@ -3,53 +3,68 @@ import tiktoken
 import multiprocessing as mp
 import os
 import numpy as np
+import pandas as pd
+from tqdm import tqdm
+from tqdm import tqdm
 
 download_folder = snapshot_download(
                 "HuggingFaceFW/fineweb", 
                 repo_type="dataset",
-                local_dir="./fineweb/",
+                local_dir="./data/fineweb/",
                 # replace "data/CC-MAIN-2023-50/*" with "sample/100BT/*" to use the 100BT sample
-                allow_patterns="sample/10BT/01*")
+                allow_patterns="sample/10BT/*")
+
 
 encoder = tiktoken.encoding_for_model("gpt2")
+eot = encoder._special_tokens['<|endoftext|>']
 
+def tokenize(row):
+    # read parquet file and get text from "text" column
+    dataset = row["text"]
 
-def tokenize(file_name):
-    with open(file_name, "r") as f:
-        dataset = f.read()
-    dataset_tokens = encoder.encode(dataset)
-    dataset_tokens = dataset_tokens + [encoder.eot]  # adding eot to delimit
+    dataset_tokens = encoder.encode_ordinary(dataset)
+    dataset_tokens = dataset_tokens + [eot]  # adding eot to delimit
     dataset_tokens = np.array(dataset_tokens, dtype=np.uint16)
+    return dataset_tokens
 
+def file_generator(filenames):
+    for filename in filenames:
+        df = pd.read_parquet(filename)
+        for _, row in df.iterrows():
+            yield row
 
-filenames = os.listdir(download_folder)
-
-BATCH_SIZE = 2 # number of files in each batch
-SHARD_SIZE = int(1e7) # 10M tokens per shard
+FILE_PATH = f"{download_folder}/sample/10BT/"
+filenames = os.listdir(FILE_PATH)
+filenames = [FILE_PATH + f for f in filenames]
+SHARD_SIZE = int(1e8) # 10M tokens per shard
 # multiprocessing
 curr_tokens = np.empty((SHARD_SIZE,), dtype=np.uint16)
 curr_index = 0
 curr_shard = 0
-for i in range(0, len(filenames), BATCH_SIZE):
-    with mp.Pool(mp.cpu_count()) as p:
-        for tokens in p.imap(tokenize, filenames, chunksize=16):
-            remaining_tokens = None
-            if len(tokens) > (SHARD_SIZE - curr_index):
-                remaining_tokens = tokens[SHARD_SIZE - curr_index:]
-                tokens = tokens[:SHARD_SIZE - curr_index]
-            curr_tokens[curr_index:curr_index + len(tokens)] = tokens
-            curr_index += len(tokens)
-            if remaining_tokens is not None:
-                # writing current shard
-                split = "val" if curr_shard == 0 else "train"
-                file_dir = f"./fineweb/{split}/"
-                os.makedirs(file_dir, exist_ok=True)
-                np.save(f"./fineweb/{split}/{curr_shard}.npy", curr_tokens)
-                curr_shard += 1
-                curr_tokens = np.empty((SHARD_SIZE,), dtype=np.uint16)
-                # filling remaining tokens
-                curr_tokens[:len(remaining_tokens)] = remaining_tokens
-                curr_index = len(remaining_tokens)
+progress_bar = None
+with mp.Pool(mp.cpu_count()) as p:
+    for tokens in p.imap(tokenize, file_generator(filenames), chunksize=16):
+        remaining_tokens = None
+        if len(tokens) > (SHARD_SIZE - curr_index):
+            remaining_tokens = tokens[SHARD_SIZE - curr_index:]
+            tokens = tokens[:SHARD_SIZE - curr_index]
+        curr_tokens[curr_index:curr_index + len(tokens)] = tokens
+        curr_index += len(tokens)
+        if progress_bar is None:
+            progress_bar = tqdm(total=SHARD_SIZE, desc=f"Shard {curr_shard}: ", unit="tokens")
+        progress_bar.update(curr_index)
+        if remaining_tokens is not None:
+            # writing current shard
+            split = "val" if curr_shard == 0 else "train"
+            file_dir = f"./fineweb/{split}/"
+            os.makedirs(file_dir, exist_ok=True)
+            np.save(f"./fineweb/{split}/{curr_shard}.npy", curr_tokens)
+            curr_shard += 1
+            curr_tokens = np.empty((SHARD_SIZE,), dtype=np.uint16)
+            # filling remaining tokens
+            curr_tokens[:len(remaining_tokens)] = remaining_tokens
+            curr_index = len(remaining_tokens)
+            progress_bar = None
 
 # writing last shard
 if curr_index > 0:
--- train_gpt2.py
+++ train_gpt2.py
@@ -3,20 +3,23 @@ from transformers.models.gpt2.configuration_gpt2 import GPT2Config
 import tiktoken
 import torch
 from torch.nn import CrossEntropyLoss
-from torch.optim import Adam
+from torch.optim import AdamW
 import time
+import os
+import numpy as np
 
 torch.manual_seed(42)
 
 encoder = tiktoken.encoding_for_model("gpt2")
+eot = encoder._special_tokens['<|endoftext|>']
 config = GPT2Config(vocab_size=50304)
 model = GPT2(config)
 model = torch.compile(model)
 
 EPOCHS = 1
-BATCH_SIZE = 128
-MINI_BATCH_SIZE = 32 # we use gradient accumulation here.
-NUM_TOKENS = 512
+BATCH_SIZE = 64
+MINI_BATCH_SIZE = 16 # we use gradient accumulation here.
+NUM_TOKENS = 1024
 MAX_STEPS = 50
 assert BATCH_SIZE % MINI_BATCH_SIZE == 0
 GRAD_ACCUM_STEPS = BATCH_SIZE // MINI_BATCH_SIZE
@@ -24,43 +27,62 @@ print(f"grad accumulation steps: {GRAD_ACCUM_STEPS}")
 # creating dataloader
 
 class Dataloader:
-    def __init__(self, file_name, batch_size, num_tokens, encoder):
-        self.file_name = file_name
+    def __init__(self, folder_path, batch_size, num_tokens, pad_token, split = "train"):
+        self.shards_path = os.path.join(folder_path, split)
         self.batch_size = batch_size
         self.num_tokens = num_tokens
-        with open(self.file_name, "r") as f:
-            dataset = f.read()
-        self.dataset_tokens = encoder.encode(dataset)
-        self.num_batches = len(self.dataset_tokens) // (self.batch_size * self.num_tokens)
-        self.curr_index = 0
+        self.shard_files = os.listdir(self.shards_path)
+        self.pad_token = pad_token
+        self.reset()
 
-    def __len__(self):
-        return self.num_batches
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        batch_token_size = self.batch_size * self.num_tokens
-        if (self.curr_index + batch_token_size) >= len(self.dataset_tokens):
-            self.reset()
-            raise StopIteration
-        x = self.dataset_tokens[self.curr_index : self.curr_index + batch_token_size]
-        y = self.dataset_tokens[self.curr_index + 1 : self.curr_index + batch_token_size + 1]
-        self.curr_index += self.batch_size * self.num_tokens
+        batch_token_size = self.batch_size * self.num_tokens + 1
+        tokens = self.curr_shard_tokens[self.curr_index : self.curr_index + batch_token_size]
+
+        if len(tokens) < batch_token_size: # need to fetch from next shard
+            self.curr_shard_idx += 1
+            if self.curr_shard_idx >= len(self.shard_files): # reached end of all shards
+                if len(tokens) > 0: # return what we have after padding
+                    tokens = np.pad(tokens, batch_token_size - len(tokens), mode="constant", constant_values=self.pad_token)
+                    # move curr_idx to end
+                    self.curr_index = len(self.curr_shard_tokens) # this will end the loop next time 
+                else:
+                    self.reset()
+                    raise StopIteration 
+            self.curr_shard = np.load(os.path.join(self.shards_path, f"{self.shard_files[self.curr_shard_idx]}"))
+            tokens = np.concatenate(tokens, self.curr_shard[:(batch_token_size - len(tokens))], axis=0) 
+            self.curr_index = batch_token_size - len(tokens) - 1
+        else:
+            self.curr_index += batch_token_size - 1
+        x = tokens[:-1]
+        y = tokens[1:]
         x = torch.tensor(x, dtype=torch.long).view(self.batch_size, self.num_tokens)
         y = torch.tensor(y, dtype=torch.long).view(self.batch_size, self.num_tokens)
         return x, y
 
     def reset(self):
         self.curr_index = 0
+        self.curr_shard_idx = 0
+        self.curr_shard_tokens = np.load(os.path.join(self.shards_path, f"{self.shard_files[self.curr_shard_idx]}"))
+        self.remaining_tokens = None
 
-TINY_SHAKESPEARE_PATH = "data/tiny_shakespeare.txt"
-data_loader = Dataloader(TINY_SHAKESPEARE_PATH, MINI_BATCH_SIZE, NUM_TOKENS, encoder)
-print(f"Number of mini-batches: {len(data_loader)}")
-NUM_BATCHES = 50 # len(data_loader) // GRAD_ACCUM_STEPS
-print(f"Number of batches: {NUM_BATCHES}")
 
+# TINY_SHAKESPEARE_PATH = "data/tiny_shakespeare.txt"
+# data_loader = Dataloader(TINY_SHAKESPEARE_PATH, MINI_BATCH_SIZE, NUM_TOKENS, encoder)
+# print(f"Number of mini-batches: {len(data_loader)}")
+# NUM_BATCHES = 50 # len(data_loader) // GRAD_ACCUM_STEPS
+# print(f"Number of batches: {NUM_BATCHES}")
+
+FINEWEB_PATH = "data/fineweb/"
+NUM_TOKEN_TOTAL = int(1e10)
+NUM_BATCHES = NUM_TOKEN_TOTAL // (BATCH_SIZE * NUM_TOKENS)
+print(f"Number of steps: {NUM_BATCHES}")
+data_loader = Dataloader(FINEWEB_PATH, MINI_BATCH_SIZE, NUM_TOKENS, eot)
+NUM_BATCHES = 50
 
 
 device = "cpu"
@@ -76,10 +98,29 @@ model.to(device)
 # loss and optimizer
 loss = CrossEntropyLoss()
 loss.to(device)
-optimizer = Adam(model.parameters(), lr=3e-4)
-optimizer.param_groups[0]["lr"] = 3e-4
+optimizer = AdamW(model.parameters(), lr=3e-4, betas=(0.9, 0.95))
+MAX_LR = 6e-4
+WARMUP_STEPS = 24 # this we calculated from gpt 3 paper. There they use first 375 M token for a 300 B token dataset.
+COSINE_DECAY_STEPS = 16530 # again in GPT3 they use first 260B tokens out of 300B dataset. 
+
+MIN_LR = MAX_LR / 10
+INIT_LR = MAX_LR / WARMUP_STEPS
+
+def get_lr(curr_lr, step):
+    if step < WARMUP_STEPS:
+        return min(MAX_LR, curr_lr + MAX_LR / WARMUP_STEPS)
+    elif step < COSINE_DECAY_STEPS:
+        step_no = step - WARMUP_STEPS
+        total_steps = COSINE_DECAY_STEPS - WARMUP_STEPS
+        cos_value = np.cos((step_no / total_steps) * np.pi) * 0.5 + 0.5 # scaling cos to given value
+        return MIN_LR + 0.9 * MAX_LR * cos_value
+    else:
+        return MIN_LR
+
+
 
 # training loop
+lr = 0
 for epoch in range(EPOCHS):
     print(f"Epoch: {epoch}")
     data_loader_iterator = iter(data_loader)
@@ -89,11 +130,14 @@ for epoch in range(EPOCHS):
         st = time.time()
         # forward pass
         loss_scalar = 0
+        lr = get_lr(lr, step)
+        optimizer.param_groups[0]["lr"] = lr
         optimizer.zero_grad()
         for _ in range(GRAD_ACCUM_STEPS):
             try:
                 x, y = next(data_loader_iterator)
             except StopIteration:
+                print("looping back")
                 pass  # end of iterator. looping back to start
             x = x.to(device)
             y = y.to(device)
@@ -101,9 +145,9 @@ for epoch in range(EPOCHS):
                 logits = model(x)
                 loss_value = loss(logits.view(logits.shape[0] * logits.shape[1], logits.shape[-1]), y.view(y.shape[-1] * y.shape[-2]))
                 loss_value /= GRAD_ACCUM_STEPS
-                # backward pass
-                loss_value.backward()
-                loss_scalar += loss_value.item()
+            # backward pass
+            loss_value.backward()
+            loss_scalar += loss_value.item()
 
         # clip gradients
         norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
@@ -114,7 +158,6 @@ for epoch in range(EPOCHS):
             torch.cuda.synchronize()
         end = time.time()
         time_taken = end - st
-        print(f"Step: {step}, loss: {loss_scalar:.4f}, norm: {norm:.4f}, time taken: {time_taken * 1000:.4f} ms, tokens per second: {(BATCH_SIZE * NUM_TOKENS/time_taken):.4f}")
+        print(f"Step: {step}, loss: {loss_scalar:.4f}, norm: {norm:.4f}, lr: {lr:.4e}, time taken: {time_taken * 1000:.4f} ms, tokens per second: {(BATCH_SIZE * NUM_TOKENS/time_taken):.4f}")
 
 torch.save(model.state_dict(), "model.pt")
-model.state_dict()
